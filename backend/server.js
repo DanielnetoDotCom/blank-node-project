090644f4c188133341db83f5d8cd73a705853a5c
Daniel Neto <me@danielneto.com>
2025-04-09 11:51:48 -0300

    .env variables added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+// Load environment variables
+dotenv.config({ path: './.env' });
+
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.BACKEND_PORT || 5000;
 
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from backend!' });
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import process from 'process'
 
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  server: {
-    // allowedHosts: ['ticket-system.danielneto.com'],
-    host: true,  
-    port: 5001,
-  },
+export default defineConfig(({ mode }) => {
+  // Carregar as variáveis de ambiente corretamente
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    server: {
+      // allowedHosts: ['ticket-system.danielneto.com'],
+      host: true,  
+      port: parseInt(env.VITE_PORT) || 5001, // Usando a variável carregada do .env
+    },
+  }
 })
